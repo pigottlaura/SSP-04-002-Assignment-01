d22c8860e20dd6b1632644335f80d12ddc28ef9f
Laura Pigott <pigottlaura@gmail.com>
2016-02-10 15:57:56 +0000

    Creating a Login Error Page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req, res, next){
+  if(req.body.username == "Laura" && req.body.password == "password"){
+    res.send('You have successfully logged in using the username: ' + req.body.username + " and password: " + req.body.password);
+  } else{
+    next();
+  }
+});
+
+router.post('/login', function(req, res, next){
+  var err = new Error('Wrong Username or Password');
+  err.status = 401;
+  res.render("loginError", {message:"Wrong Username or Password", error: err})
+});
+
 module.exports = router;
