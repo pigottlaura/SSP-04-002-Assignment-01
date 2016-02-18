42814366fcc2ea3576fea0fddc9eb84c3907a6b9
Laura Pigott <pigottlaura@gmail.com>
2016-02-18 17:12:03 +0000

    Using cookies to confirm if the user is logged in

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,11 @@ var fs = require("fs");
 var storedSecrets;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 router.post('/login', function(req, res, next){
   if(req.body.username == "Laura" && req.body.password == "password"){
     res.cookie("username", req.body.username);
+    res.cookie("loggedIn", true);
     res.redirect("/users/secrets");
   } else{
     next();
@@ -24,32 +22,36 @@ router.post('/login', function(req, res, next){
 });
 
 router.get('/secrets', function (req, res, next){
-  console.log(req.cookies.username);
-  storedSecrets = JSON.parse(fs.readFileSync("./bin/secrets.json", "utf8"));
-  //console.log(storedSecrets);
-  res.render('secrets', {username: req.cookies.username, secrets: storedSecrets});
+  if(req.cookies.loggedIn == "true"){
+    storedSecrets = JSON.parse(fs.readFileSync("./bin/secrets.json", "utf8"));
+    //console.log(storedSecrets);
+    res.render('secrets', {username: req.cookies.username, secrets: storedSecrets});
+  } else {
+    console.log("\nThis user is not yet logged in. Returning them to the home page.\n");
+    res.redirect("/");
+  }
 });
 
 router.post('/secrets/modifySecrets', function (req, res, next){
   if(req.body.submit == "Delete this Secret"){
-    console.log("\n User wants to delete " + req.body.secretId + "\n");
+    console.log("\n User wants to delete a secret. SecretId = " + req.body.secretId + "\n");
     for(var i = 0; i < storedSecrets.length; i++){
       if(storedSecrets[i].secretId == req.body.secretId){
         storedSecrets.splice(i, 1);
       }
     }
   } else if (req.body.submit == "Keep my Secret"){
-    console.log("New Secret Recieved: " + req.body.secretTitle);
+    console.log("\nNew Secret Recieved: " + req.body.secretTitle + "\n");
     var newSecret = {secretTitle: req.body.secretTitle, secret: req.body.secret, secretId: (new Date).getTime()};
     storedSecrets.push(newSecret);
   }
   fs.writeFile("./bin/secrets.json", JSON.stringify(storedSecrets), "utf8", function(err) {
     if(err)
     {
-      console.log("Failed to save secrets " + err);
+      console.log("\nFailed to save secrets " + err + "\n");
     }
     else {
-      console.log("Secrets successfully saved :)");
+      console.log("\nSecrets successfully saved :)\n");
     }
     res.redirect("/users/secrets");
   });
