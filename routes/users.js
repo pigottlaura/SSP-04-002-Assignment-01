a6eb218d99ae67c50aaeb166a5f6ba3193d65b45
Laura Pigott <pigottlaura@gmail.com>
2016-02-18 15:40:49 +0000

    Successfully allowing user to add and delete their secrets

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
+var storedSecrets;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,25 +23,33 @@ router.post('/login', function(req, res, next){
 });
 
 router.get('/secrets', function (req, res, next){
-  var storedSecrets = fs.readFileSync("./bin/secrets.json", "utf8");
+  storedSecrets = JSON.parse(fs.readFileSync("./bin/secrets.json", "utf8"));
   //console.log(storedSecrets);
-  res.render('secrets', { username: req.body.username, secrets: JSON.parse(storedSecrets)});
+  res.render('secrets', {username: req.body.username, secrets: storedSecrets});
 });
 
-router.post('/secrets', function (req, res, next){
-  console.log("New Secret Recieved: " + req.body.secretTitle);
-  var newSecret = {secretTitle: req.body.secretTitle, secret: req.body.secret};
-  var storedSecrets = JSON.parse(fs.readFileSync("bin/secrets.json", "utf8"));
-  storedSecrets.push(newSecret);
+router.post('/secrets/modifySecrets', function (req, res, next){
+  if(req.body.submit == "Delete this Secret"){
+    console.log("\n User wants to delete " + req.body.secretId + "\n");
+    for(var i = 0; i < storedSecrets.length; i++){
+      if(storedSecrets[i].secretId == req.body.secretId){
+        storedSecrets.splice(i, 1);
+      }
+    }
+  } else if (req.body.submit == "Keep my Secret"){
+    console.log("New Secret Recieved: " + req.body.secretTitle);
+    var newSecret = {secretTitle: req.body.secretTitle, secret: req.body.secret, secretId: (new Date).getTime()};
+    storedSecrets.push(newSecret);
+  }
   fs.writeFile("./bin/secrets.json", JSON.stringify(storedSecrets), "utf8", function(err) {
     if(err)
     {
-      console.log("Failed to save new secret" + err);
+      console.log("Failed to save secrets " + err);
     }
     else {
-      console.log("New " + newSecret.secretTitle + " successfully saved :)");
-      res.redirect('/users/secrets');
+      console.log("Secrets successfully saved :)");
     }
+    res.redirect("/users/secrets");
   });
 });
 
