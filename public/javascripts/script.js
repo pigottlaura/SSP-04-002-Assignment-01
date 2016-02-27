6acf8e7c0a47953b9b165529282705de4a343fd1
Laura Pigott <pigottlaura@gmail.com>
2016-02-27 16:39:17 +0000

    Creating interface to edit secrets
    Server side functionality not yet added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,32 @@
 jQuery(document).ready(function($){
+    $(".updateSecret").hide();
+    
     $( "#accordion" )
       .accordion({
         header: "> div > h3",
-        collapsible: true
+        collapsible: true,
+        heightStyle: "content"
       })
+    $(".editSecret").click(function(){
+        var currentSecretText = $(this).prev().text();
+        console.log("Secret " + this.id + " Clicked - " + currentSecretText);
+        
+        $(this).siblings(".updateSecret").show().css("display:block");
+        
+        $(this).siblings(".deleteSecret").hide();
+        $(this).siblings(".secretText").hide();
+        $(this).hide();
+    });
+    $(".cancel").click(function(){
+        console.log("Cancel clicked");
+        var originalSecretValue = $(this).parent().siblings(".secretText").text();
+        $(this).parent().hide();
+        $(this).parent().siblings(".updateSecret").hide();
+        $(this).siblings(".newSecretText").val(originalSecretValue);
+        
+        $(this).parent().siblings(".deleteSecret").show();
+        $(this).parent().siblings(".secretText").show();
+        $(this).parent().siblings(".editSecret").show();
+    });
 });
+        
