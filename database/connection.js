a553b36f8fd155fc508c7e90f5471a2daf62884b
Laura Pigott <pigottlaura@gmail.com>
2016-02-28 02:02:50 +0000

    Trying to get Remote Database working without putting password details into GitHub
    Up until now, I have been able to run the remote Azure database by inputing the connection string host name, username and password, but as I would prefer not to have to put these on GitHub, I am now trying out a method from https://azure.microsoft.com/en-us/documentation/articles/web-sites-dotnet-deploy-aspnet-mvc-app-membership-oauth-sql-database/

--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,22 @@
 var mysql = require('mysql');
 
-// Setting up the connection to my local mySql database (running on a WAMP server)
-var connection = mysql.createConnection({
-    host: "localHost",
-    user: "root",
-    password: "",
-    database: "mySecrets"
-});
+// Creating a connection string varaible, which will allow me to access my local
+// database when running my app locally (for testing purposes) or my remote
+// MySQL database on Azure, when my application has been deployed live to Azure
+var connectionString = process.env.CUSTOMCONNSTR_MySecrets || "mySecrets://localhost/";
+
+// Setting up the connection to my MySQL database (running on a WAMP server locally,
+// or live on Azure, depending on where my application is being run from)
+if (connectionString == "mySecrets://localhost/") {
+    var connection = mysql.createConnection({
+        host: "localHost",
+        user: "root",
+        password: "",
+        database: "mySecrets"
+    });
+} else {
+    var connection = mysql.createConnection(connectionString);
+}
 
 //connection.end();
 
