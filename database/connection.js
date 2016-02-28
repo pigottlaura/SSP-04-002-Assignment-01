21b4d48891c9b339f1e59da7d0506768357f5cc2
Laura Pigott <pigottlaura@gmail.com>
2016-02-28 03:31:44 +0000

    trying process.env.DATABASE

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 // Creating a connection string varaible, which will allow me to access my local
 // database when running my app locally (for testing purposes) or my remote
 // MySQL database on Azure, when my application has been deployed live to Azure
-var connectionString = process.env.MYSQLCONNSTR_mySecrets || "mysql://root:@localhost/mySecrets";
+var connectionString = process.env.DATABASE_mySecrets || "mysql://root:@localhost/mySecrets";
 
 // Setting up the connection to my MySQL database (running on a WAMP server locally,
 // or live on Azure, depending on where my application is being run from)
